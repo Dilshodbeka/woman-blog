4b107e3309eace87ac9134c1a1a2316bb9e88320
Dilshodbeka <abdullayev.dima7080@gmail.com>
2024-03-26 12:33:05 +0500

    login logout view

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 
 
-class LoginUserForm(forms.Form):
-    username = forms.CharField(label="LOGIN",
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label="Login",
                                widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label="PASSWORD",
+    password = forms.CharField(label="Parol",
                                widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password']
